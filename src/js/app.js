09511ce3e14d601852bf29de46f85b214498801b
Mikael Karon <mikael@karon.se>
2012-02-24 18:33:11 +0800

    refactored appstart

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,24 @@
 require({
-	"baseUrl": "js",
-	"paths": {
-		"jquery": "lib/jquery/dist/jquery",
-		"compose": "lib/composejs/compose",
-		"deferred": "lib/troopjs-jquery/src/deferred",
-		"troopjs": "lib/troopjs/src"
+	"baseUrl" : "js",
+	"paths" : {
+		"jquery" : "lib/jquery/dist/jquery",
+		"compose" : "lib/composejs/compose",
+		"deferred" : "lib/troopjs-jquery/src/deferred",
+		"troopjs" : "lib/troopjs/src",
+		"troopjs-jquery" : "lib/troopjs-jquery/src"
 	}
-}, [ "jquery", "troopjs/component/widget", "troopjs/pubsub/hub" ], function App(jQuery, Widget, hub) {
+}, [ "jquery", "troopjs/component/widget", "troopjs/pubsub/hub", "troopjs-jquery/weave", "troopjs-jquery/wire" ], function App(jQuery, Widget, hub) {
 	jQuery(document).ready(function ready($) {
-		$.Deferred(function deferredStart(dfdStart) {
-			var widget = new Widget(this);
+		var element = this.body;
 
-			dfdStart.resolve(widget);
+		$.Deferred(function deferredStart(dfdStart) {
+			var widget = new Widget(element, "widget/body");
+
+			widget.weave(widget.$element, dfdStart);
+
+			console.log(widget);
 		}).done(function doneStart(widget) {
-			
+			console.log(arguments);
 		});
 	});
 });
