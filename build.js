2fc038ba733f0e2c6c2e39f0e1855276d5751bd1
Mikael Karon <mikael@karon.se>
2012-02-27 18:13:42 +0800

    added build.js

---
+++ build.js
@@ -0,0 +1,24 @@
+({
+	"baseUrl" : "js",
+	"paths" : {
+		"jquery" : "lib/jquery/dist/jquery",
+		"compose" : "lib/composejs/compose",
+		"deferred" : "lib/troopjs-jquery/src/deferred",
+		"text" : "lib/requirejs/text",
+		"template" : "lib/troopjs-require/src/template",
+		"detect" : "lib/troopjs-require/src/detect",
+		"troopjs" : "lib/troopjs/src",
+		"troopjs-jquery" : "lib/troopjs-jquery/src"
+	},
+
+	appDir : "./src/",
+
+	fileExclusionRegExp: "lib/troopjs",
+
+	modules : [ {
+		name : "app",
+		include : [
+			"widget/display/panel"
+		]
+	} ]
+})
