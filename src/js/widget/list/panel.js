90fc0e6cea79dc32043ffdc0fda1a1d2ad3a16e2
Mikael Karon <mikael@karon.se>
2012-02-29 01:19:33 +0800

    start implementation of todo

---
+++ src/js/widget/list/panel.js
@@ -0,0 +1,33 @@
+define( [ "troopjs/component/widget", "jquery", "template!./panel.html" ], function ListPanelModule(Widget, $, template) {
+	var data = [{
+			"id": 1,
+			"completed": false,
+			"text": "Work"
+		}, {
+			"id": 2,
+			"completed": true,
+			"text": "Play"
+		}, {
+			"id": 3,
+			"completed": true,
+			"text": "Sleep"
+		}];
+
+	return Widget.extend(function ListPanelWidget(element, name) {
+		this.html(template, data);
+	}, {
+		"dom/action/status": function onStatus(topic, $event, id) {
+			console.log("status", id);
+		},
+
+		"dom/action/delete": function onDelete(topic, $event, id) {
+			console.log("delete", id);
+		},
+
+		"dom/action/edit.keyup": function onEditKeyUp(topic, $event, id) {
+			console.log("edit", id);			
+		},
+
+		"dom/action.keyup": function () {}
+	});
+});
