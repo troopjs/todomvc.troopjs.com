2ed65be1c0155a5e097b87a04537424167da1523
Mikael Karon <mikael@karon.se>
2012-02-29 01:20:10 +0800

    remove unused widget

--- src/js/widget/display/panel.js
+++
@@ -1,7 +0,0 @@
-define( [ "troopjs/component/widget", "jquery" ], function DisplayPanelModule(Widget, $) {
-	return Widget.extend({
-		"dom/action/test": function ActionTest(topic, $event) {
-			console.log(arguments);
-		}
-	});
-});
