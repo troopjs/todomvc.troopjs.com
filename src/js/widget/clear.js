4c98b9775c16a1793ea93de4b0af11d8b104069a
Mikael Karon <mikael@karon.se>
2012-03-13 17:15:17 +0800

    don't send id's as an argument to clear action

--- src/js/widget/clear.js
+++ src/js/widget/clear.js
@@ -1,32 +1,28 @@
 define( [ "troopjs/component/widget", "jquery" ], function ClearModule(Widget, $) {
 
-	var IDS = "ids";
-
 	function filter(item, index) {
-		return item === null ? null : item.completed ? index : null;
+		return item === null || !item.completed;
 	}
 
 	return Widget.extend({
 		"hub/todos/change" : function onChange(topic, items) {
-			var ids = $.map(items, filter);
-			var count = ids.length;
+			var count = $.grep(items, filter, true).length;
 			var $element = this.$element;
 
 			if (count > 0) {
 				$element
-					.data(IDS, ids)
 					.text("Clear " + count + (count > 1 ? " completed items" : " completed item"))
-					.show("fast");
+					.show();
 			}
 			else {
 				$element
-					.removeData(IDS)
 					.text("Clear no completed items")
-					.hide("fast");
+					.hide();
 			}
 		},
 
-		"dom/action/clear" : function onClear(topic, $event, ids) {
+		"dom/action/clear" : function onClear(topic, $event) {
+			this.publish("todos/clear");
 		},
 
 		"dom/action.click" : $.noop
