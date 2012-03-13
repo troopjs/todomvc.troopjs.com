da1c9d91aba4b492386e4e64aaeb0da430028463
Mikael Karon <mikael@karon.se>
2012-03-13 14:47:09 +0800

    update module name

--- src/js/widget/display.js
+++ src/js/widget/display.js
@@ -1,4 +1,4 @@
-define( [ "troopjs/component/widget", "jquery" ], function MainModule(Widget, $) {
+define( [ "troopjs/component/widget", "jquery" ], function DisplayModule(Widget, $) {
 	return Widget.extend({
 		"hub/todos/count": function onCount(topic, count) {
 			this.$element[count > 0 ? "show" : "hide"]("fast");
