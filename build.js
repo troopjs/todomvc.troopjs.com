2b26a90b105c51b7e7f9be11318e7156dc05942f
Mikael Karon <mikael@karon.se>
2012-03-20 17:35:12 +0800

    update build configuration

--- build.js
+++ build.js
@@ -6,20 +6,23 @@
 		"compose" : "lib/composejs/compose",
 		"deferred" : "lib/troopjs-jquery/src/deferred",
 		"text" : "lib/requirejs/text",
-		"template" : "lib/troopjs-require/src/template",
-		"detect" : "lib/troopjs-require/src/detect",
+		"template" : "lib/troopjs-requirejs/src/template",
+		"detect" : "lib/troopjs-requirejs/src/detect",
 		"troopjs" : "lib/troopjs/src",
 		"troopjs-jquery" : "lib/troopjs-jquery/src"
 	},
 
 	appDir : "./src/",
 
-	fileExclusionRegExp: "lib/troopjs",
-
 	modules : [ {
 		name : "app",
 		include : [
-			"widget/display/panel"
+			"widget/clear",
+			"widget/count",
+			"widget/create",
+			"widget/display",
+			"widget/list",
+			"widget/mark"
 		]
 	} ]
 })
