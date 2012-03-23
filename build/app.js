8574c871a14b0bc1d86c57d7b59363dfee647d23
Mikael Karon <mikael@karon.se>
2012-03-23 15:17:38 +0800

    working (but not what I want) build

--- build/app.js
+++ build/app.js
@@ -2,7 +2,12 @@
 	"baseUrl" : "js",
 	"paths" : {
 		"jquery" : "lib/jquery/dist/jquery",
-		"troopjs-bundle" : "lib/troopjs-bundle/dist/troopjs-bundle-mini"
+		"compose" : "lib/troopjs-bundle/src/lib/composejs/compose",
+		"deferred" : "lib/troopjs-bundle/src/lib/troopjs-jquery/src/deferred",
+		"text" : "lib/troopjs-bundle/src/lib/requirejs/text",
+		"template" : "lib/troopjs-bundle/src/lib/troopjs-requirejs/src/template",
+		"troopjs" : "lib/troopjs-bundle/src/lib/troopjs/src",
+		"troopjs-jquery" : "lib/troopjs-bundle/src/lib/troopjs-jquery/src"
 	},
 
 	"appDir" : "../src/",
