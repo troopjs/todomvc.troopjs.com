32d170c8eafa07aee386005b30d1f3ad58ea82d0
Mikael Karon <mikael@karon.se>
2012-03-23 12:55:40 +0800

    use troopjs-bundle

--- src/js/app.js
+++ src/js/app.js
@@ -2,22 +2,10 @@ require({
 	"baseUrl" : "js",
 	"paths" : {
 		"jquery" : "lib/jquery/dist/jquery",
-		"compose" : "lib/composejs/compose",
-		"deferred" : "lib/troopjs-jquery/src/deferred",
-		"text" : "lib/requirejs/text",
-		"template" : "lib/troopjs-requirejs/src/template",
-		"detect" : "lib/troopjs-requirejs/src/detect",
-		"troopjs" : "lib/troopjs/src",
-		"troopjs-jquery" : "lib/troopjs-jquery/src"
-	}
-}, [
-	"jquery",
-	"troopjs-jquery/action",
-	"troopjs-jquery/destroy",
-	"troopjs-jquery/dimensions",
-	"troopjs-jquery/hashchange",
-	"troopjs-jquery/weave",
-	"troopjs-jquery/wire" ], function App(jQuery) {
+		"troopjs-bundle" : "lib/troopjs-bundle/dist/troopjs-bundle-mini"
+	},
+	"deps": [ "troopjs-bundle" ]
+}, [ "jquery" ], function App(jQuery) {
 	jQuery(document).ready(function ready($) {
 		var body = this.body;
 
