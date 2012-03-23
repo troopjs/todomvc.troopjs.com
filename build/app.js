9b590e31765a0c69d8088b54dff25e9f411e16c1
Mikael Karon <mikael@karon.se>
2012-03-23 14:41:07 +0800

    attempt to fix build. not working.

--- build/app.js
+++ build/app.js
@@ -2,26 +2,18 @@
 	"baseUrl" : "js",
 	"paths" : {
 		"jquery" : "lib/jquery/dist/jquery",
-		"compose" : "lib/composejs/compose",
-		"deferred" : "lib/troopjs-jquery/src/deferred",
-		"text" : "lib/requirejs/text",
-		"template" : "lib/troopjs-requirejs/src/template",
-		"detect" : "lib/troopjs-requirejs/src/detect",
-		"troopjs" : "lib/troopjs/src",
-		"troopjs-jquery" : "lib/troopjs-jquery/src"
+		"troopjs-bundle" : "lib/troopjs-bundle/dist/troopjs-bundle-mini"
 	},
 
 	appDir : "../src/",
 
-	modules : [ {
-		name : "app",
-		include : [
-			"widget/clear",
-			"widget/count",
-			"widget/create",
-			"widget/display",
-			"widget/list",
-			"widget/mark"
-		]
-	} ]
+	name : "app",
+	include : [
+		"widget/clear",
+		"widget/count",
+		"widget/create",
+		"widget/display",
+		"widget/list",
+		"widget/mark"
+	]
 })
