b51a3453207daae742e6a579148696e5de16ed57
Mikael Karon <mikael@karon.se>
2012-03-23 18:09:27 +0800

    provide template/text as separate dependencies
    use deps in optimizer

--- build/app.js
+++ build/app.js
@@ -2,20 +2,15 @@
 	"baseUrl" : "js",
 	"paths" : {
 		"jquery" : "lib/jquery/dist/jquery",
-		"compose" : "lib/troopjs-bundle/src/lib/composejs/compose",
-		"deferred" : "lib/troopjs-bundle/src/lib/troopjs-jquery/src/deferred",
-		"text" : "lib/troopjs-bundle/src/lib/requirejs/text",
 		"template" : "lib/troopjs-bundle/src/lib/troopjs-requirejs/src/template",
-		"troopjs" : "lib/troopjs-bundle/src/lib/troopjs/src",
-		"troopjs-jquery" : "lib/troopjs-bundle/src/lib/troopjs-jquery/src"
+		"text" : "lib/troopjs-bundle/src/lib/requirejs/text",
+		"troopjs-bundle" : "lib/troopjs-bundle/dist/troopjs-bundle-mini"
 	},
+	"deps": [ "troopjs-bundle" ],
 
 	"appDir" : "../src/",
 
 	"modules": [{
-		"name" : "troopjs-bundle",
-		"create" : true
-	}, {
 		"name" : "app",
 		"include" : [
 			"widget/clear",
