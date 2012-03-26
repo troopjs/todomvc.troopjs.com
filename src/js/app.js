ddcb7420bfa35f5e86dd319ee17d4fe7c162dcee
Mikael Karon <mikael@karon.se>
2012-03-26 15:46:01 +0800

    add config to priority

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ require({
 		"jquery" : "lib/jquery/dist/jquery",
 		"troopjs-bundle" : "lib/troopjs-bundle/dist/troopjs-bundle"
 	},
-	"priority": [ "jquery", "troopjs-bundle" ]
+	"priority": [ "jquery", "config", "troopjs-bundle" ]
 }, [ "jquery" ], function App(jQuery) {
 	jQuery(document).ready(function ready($) {
 		$(this.body).find("[data-weave]").weave();
