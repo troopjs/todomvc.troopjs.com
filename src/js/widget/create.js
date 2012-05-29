d5d52d1a87ef1304bba45a088feb7dd8759eefac
Mikael Karon <mikael@karon.se>
2012-05-29 17:24:30 +0800

    trim create

--- src/js/widget/create.js
+++ src/js/widget/create.js
@@ -1,14 +1,22 @@
 define( [ "troopjs-core/component/widget" ], function CreateModule(Widget) {
+	var RE = /^\s+|\s+$/;
+	var EMPTY = "";
+
 	return Widget.extend({
 		"dom/keyup" : function onKeyUp(topic, $event) {
 			var self = this;
 			var $element = self.$element;
+			var value;
 
 			switch($event.keyCode) {
 			case 13:
-				self.publish("todos/add", $element.val());
+				value = $element.val().replace(RE, EMPTY);
 
-				$element.val("");
+				if (value !== EMPTY) {
+					self.publish("todos/add", value);
+
+					$element.val(EMPTY);
+				}
 			}
 		}
 	});
