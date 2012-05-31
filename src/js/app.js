ec8167b6114346d01c1df6aef606fdbecb44aef4
Mikael Karon <mikael@karon.se>
2012-05-31 20:28:10 +0800

    use `deps` instead of `priority` (as 2.0 does not support `priority`)

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ require({
 		"jquery" : "lib/jquery/dist/jquery",
 		"troopjs-bundle" : "lib/troopjs-bundle/dist/troopjs-bundle"
 	},
-	"priority": [ "jquery", "config", "troopjs-bundle" ]
+	"deps": [ "troopjs-bundle" ]
 }, [ "jquery", "widget/application" ], function App(jQuery, Application) {
 	jQuery(document).ready(function ready($) {
 		Application($(this.body), "app/todos").start();
