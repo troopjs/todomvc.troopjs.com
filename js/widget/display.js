a942e2fc6de52ee8705f17f6335d1e62458886c8
Mikael Karon <mikael@karon.se>
2013-05-27 15:06:42 +0800

    Use .toggle instead of ["methodName"]

--- js/widget/display.js
+++ js/widget/display.js
@@ -8,7 +8,7 @@ define([ "troopjs-browser/component/widget", "jquery" ], function DisplayModule(
 
 	return Widget.extend({
 		"hub:memory/todos/change": function onChange(items) {
-			this.$element[$.grep(items, filter).length > 0 ? "show" : "hide"]();
+			this.$element.toggle($.grep(items, filter).length > 0);
 		}
 	});
 });
