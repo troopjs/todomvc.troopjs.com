cdbb2314e568dbd47e4d45bac5499dd7f7953955
Garry Yao <yaojun85@gmail.com>
2014-07-29 12:18:57 +0800

    coding style fixes

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
 "use strict";
 
 require.config({
-	"baseUrl": "bower_components",
-	"packages": [
+	"baseUrl" : "bower_components",
+	"packages" : [
 		{
-			name: 'jquery',
-			main: 'dist/jquery.js'
+			name : "jquery",
+			main : "dist/jquery.js"
 		},
 		{
-			name: 'poly',
-			main: 'poly.js'
+			name : "poly",
+			main : "poly.js"
 		},
 		{
-			name: 'requirejs',
-			main: 'require.js'
+			name : "requirejs",
+			main : "require.js"
 		},
 		{
-			name: 'when',
-			main: 'when.js'
+			name : "when",
+			main : "when.js"
 		},
 		{
-			name: 'troopjs-todos',
-			location: '../'
+			name : "troopjs-todos",
+			location : "../"
 		}
 	],
 
-	"map": {
-		"*": {
-			"template": "mu-template/plugin",
-			"text": "requirejs-text/text",
-			"config": "app/config"
+	"map" : {
+		"*" : {
+			"template" : "mu-template/plugin",
+			"text" : "requirejs-text/text",
+			"config" : "app/config"
 		}
 	},
 
-	"deps": [ "require", "jquery", "when/monitor/console" ],
+	"deps" : ["require", "jquery", "when/monitor/console"],
 
-	"callback": function Boot(contextRequire, jQuery) {
-		contextRequire([ "troopjs-dom/application/widget", "troopjs-dom/hash/widget", "troopjs-dom/loom/plugin"],
-			function Strap(Application, RouteWidget) {
-				jQuery(function($) {
-					Application($("html"), "bootstrap", RouteWidget($(window))).start();
-				});
+	"callback" : function Boot(contextRequire, jQuery) {
+		contextRequire([
+			"troopjs-dom/application/widget",
+			"troopjs-dom/hash/widget",
+			"troopjs-dom/loom/plugin"
+		], function Strap(Application, RouteWidget) {
+			jQuery(function ($) {
+				Application($("html"), "bootstrap", RouteWidget($(window))).start();
 			});
+		});
 	}
 });
